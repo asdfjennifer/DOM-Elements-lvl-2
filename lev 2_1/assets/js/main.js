874fafcc563dce4a15480ba9757493fd9543f022
asdfjennifer <jennifergenger@gmailcom>
2022-05-16 22:11:31 +0200

    DOM Elements lvl 2

---
+++ lev 2_1/assets/js/main.js
@@ -0,0 +1,10 @@
+// console.log("working?");
+
+let navHome = document.getElementById('navHome');
+
+function change() {
+    // console.log("change");
+    navHome.classList.add("change");
+}
+
+// change();
---
+++ lev 2_2/assets/js/main.js
@@ -0,0 +1,35 @@
+// // console.log("working?");
+
+// let vorname = document.getElementById("vorname");
+// let name = document.getElementById("name");
+// let land = document.getElementById("land");
+
+// // function select() {
+// //     console.log("workinggg");
+// // }
+
+// function select() {
+//     // console.log("worksss");
+//     // document.getElementById("vorname");
+//     // console.log("Full name: " + document.getElementById("vorname").value + " " + document.getElementById("nachname").value + " Land: " + document.getElementById("land").value);
+//     // console.log(document.getElementById("vorname").value);
+//     // document.getElementById(("nachname").onclick);
+//     console.log(vorname.value);
+//     console.log(name.value);
+//     console.log(land.value);
+// }
+
+// select();
+
+let firstName = document.getElementById("vorname");
+let lastName = document.getElementById("name");
+let country = document.getElementById("land");
+
+function select() {
+        console.log(vorname.value);
+        console.log(name.value);
+        console.log(land.value);
+    }
+    
+
+    select();
